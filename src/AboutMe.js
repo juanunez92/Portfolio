668ab4611615cc82cd780457a9f83d5a2ef7c670
Juan <Juan@gmail.com>
2024-02-09 01:43:56 +0100

    add new component

---
+++ src/AboutMe.js
@@ -0,0 +1,13 @@
+// En AboutMe.js
+import React from 'react';
+
+const AboutMe = () => {
+    return (
+        <div>
+            <h2>Sobre Mí</h2>
+            <p>¡Hola! Soy [tu nombre]. Bienvenido/a a mi portfolio...</p>
+        </div>
+    );
+}
+
+export default AboutMe;

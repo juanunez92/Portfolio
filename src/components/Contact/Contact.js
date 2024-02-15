92abd0568ee866548ddfd35450a780f1dcc62034
Juanan <juanunez92@gmail.com>
2024-02-15 19:53:12 +0100

    Form and Fonts

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,14 +1,40 @@
 // Contact.js
-import React from 'react';
+import React from "react";
+import "./contact.css";
 
 const Contact = () => {
-    return (
-        <section id="contact">
-            <h2>Contact</h2>
-            <p>Puedes contactarme en la siguiente dirección de correo electrónico: example@example.com</p>
-            {/* Agrega más información de contacto según sea necesario */}
-        </section>
-    );
-}
-
+  return (
+    <div className="contact-form" id="contact">
+      <h2 className="contact-tittle">Send Me an Email!!!</h2>
+      <p className="contact-description">
+        Send me an email to this address
+        <a href="mailto:juanunez92@gmail.com"> juanunez92@gmail.com </a> or fill
+        in the form.
+      </p>
+      <form>
+        <div className="form-group">
+          <label htmlFor="name">Nombre:</label>
+          <input type="text" id="name" name="name" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Mensaje:</label>
+          <textarea id="message" name="message" rows="4"></textarea>
+        </div>
+        <button type="submit">Enviar</button>
+      </form>
+      <div className="social-links">
+        <h3>Social Media</h3>
+        <ul>
+            <li><a href="https://www.linkedin.com/in/jnuneznun/" target="_blank" rel="noreferrer">LinkedIn</a></li>
+            <li><a href="https://www.behance.net/juanannunezn" target="_blank" rel="noreferrer">Behance</a></li>
+            <li><a href="https://github.com/juanunez92" target="_blank"rel="noreferrer">GitHub</a></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 export default Contact;
